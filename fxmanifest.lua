c2a81ffc61bdfca2dd5478c0592c9d1f5ba5b2ec
momo.agent <maxanit93@gmail.com>
2025-09-13 15:43:26 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,10 +13,10 @@ loadscreen_manual_shutdown "yes"
 loadscreen_cursor "yes"
 
 files({
-    'ui/index.html',
-    'ui/style.css',
-    'ui/script.js',
-    'ui/img/*.png',
-    'ui/img/*.jpg',
-    'ui/music/*.mp3',
+    'index.html',
+    'style.css',
+    'script.js',
+    'img/*.png',
+    'img/*.jpg',
+    'music/*.mp3',
 });
